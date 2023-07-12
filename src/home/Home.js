85120a94b92363fbabee371618db9dfe37855fd2
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-12 10:59:20 -0600

    Updated Home component to have a nav bar with Generations

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Home(){
     return (
-        <h1>Pokemon Pokedex</h1>
+        <>
+            <div className="mt-4">
+                <h1>Pokemon Pokedex</h1>
+            </div>
+            <div className="text-center">
+                <div className="card-header">
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link to="#">Generations</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
 
