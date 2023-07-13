bd5bc4623af0589697d8edb8cb40316f86d732e0
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-13 11:29:35 -0600

    Fixed margin for GenSevenPokemon and GenSixPokemon

--- src/generations/GenSixPokemon.js
+++ src/generations/GenSixPokemon.js
@@ -15,7 +15,7 @@ function GenSixPokemon({pokemon}){
 
     return (
         <>
-            <div className="border p-2 me-2 mb-20">
+            <div className="border p-2 me-2 mb-2">
                 <Link to="#">{pokemon.name}</Link>
                 {sprite ? <img src={`${sprite.sprites.front_default}`} alt={`${pokemon.name} sprite`} /> : <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="ditto" /> }
             </div>
