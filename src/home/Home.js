44ba7722b4d2537a123568b0b2226a5663059d0f
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-18 17:08:59 -0600

    Added a carousel of images to the Home component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,10 +1,89 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-      <div className="d-flex justify-content-center mt-4">
-        <h1>Pokemon Pokedex</h1>
+      <div className="d-flex flex-column align-items-center justify-content-center mt-4">
+        <h1>Welcome to the World of Pokemon!</h1>
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="https://wallpaperaccess.com/full/8611.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://wallpaperaccess.com/full/20996.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://wallpaper.dog/large/754223.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+        <div>
+          <h1>Pokemon of the Day</h1>
+        </div>
       </div>
     </>
   );
