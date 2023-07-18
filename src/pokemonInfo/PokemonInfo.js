0e47c76c234111080dac9647d2d44a24af1875ac
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-18 15:58:25 -0600

    Added styling to PokemonInfo and background i

--- src/pokemonInfo/PokemonInfo.js
+++ src/pokemonInfo/PokemonInfo.js
@@ -24,9 +24,15 @@ function PokemonInfo() {
     return (
       <>
         <div className="p-5">
-          <div className="border border-dark d-flex flex-column m-5 p-5">
+          <div
+            className="rounded-4 border border-3 border-dark d-flex flex-column m-5 p-5"
+            id="backgroundImage"
+          >
             <div className="d-flex flex-row justify-content-evenly mb-4">
-              <div className="d-flex align-items-center w-25 justify-content-center">
+              <div
+                className="rounded-4 d-flex align-items-center w-25 justify-content-center mt-5 mb-5"
+                id="pokemonImage"
+              >
                 <img
                   className="w-100"
                   src={`${pokemon.sprites.front_default}`}
@@ -34,24 +40,45 @@ function PokemonInfo() {
                 />
               </div>
               <div>
-                <h5>Name: {pokemon.name}</h5>
-                <p>Type: {pokemon.types[0].type.name}</p>
-                <div className="border-top border-end border-start">
+                <h2
+                  className="border border-danger rounded-4 p-1 d-flex justify-content-center"
+                  id="nameTag"
+                >
+                  {pokemon.name} #{pokemon.id}
+                </h2>
+                <h5
+                  className="border border-light-subtle rounded-4 p-1 d-flex justify-content-center"
+                  id="typeNormalTag"
+                >
+                  Type: {pokemon.types[0].type.name}
+                </h5>
+                <div
+                  className=" mb-5 p-2 border-top border-end border-start rounded-4"
+                  id="tableColor"
+                >
                   <table className="table">
                     <thead>
                       <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Stats</th>
-                        <th scope="col">Base Value</th>
+                        <th scope="col" id="tableColor">
+                          #
+                        </th>
+                        <th scope="col" id="tableColor">
+                          Stats
+                        </th>
+                        <th scope="col" id="tableColor">
+                          Base Value
+                        </th>
                       </tr>
                     </thead>
                     <tbody className="tbody">
                       {pokemon.stats.map((stat, index) => {
                         return (
                           <tr key={index}>
-                            <th scope="row">{index + 1}</th>
-                            <td>{stat.stat.name}</td>
-                            <td>{stat.base_stat}</td>
+                            <th scope="row" id="tableColor">
+                              {index + 1}
+                            </th>
+                            <td id="tableColor">{stat.stat.name}</td>
+                            <td id="tableColor">{stat.base_stat}</td>
                           </tr>
                         );
                       })}
@@ -60,25 +87,34 @@ function PokemonInfo() {
                 </div>
               </div>
             </div>
-            <div className="justify-content-center">
-              <div className="scrollTable border">
+            <div
+              className="justify-content-center p-2 rounded-4"
+              id="tableColor"
+            >
+              <div className="scrollTable">
                 <table className="table">
                   <thead>
                     <tr>
-                      <th scope="col">Move Name</th>
-                      <th scope="col">Level Learned At</th>
-                      <th scope="col">Learned Method</th>
+                      <th scope="col" id="tableColor">
+                        Move Name
+                      </th>
+                      <th scope="col" id="tableColor">
+                        Level Learned At
+                      </th>
+                      <th scope="col" id="tableColor">
+                        Learned Method
+                      </th>
                     </tr>
                   </thead>
                   <tbody className="tbody">
                     {pokemon.moves.map((move, index) => {
                       return (
                         <tr key={index}>
-                          <td>{move.move.name}</td>
-                          <td>
+                          <td id="tableColor">{move.move.name}</td>
+                          <td id="tableColor">
                             {move.version_group_details[0].level_learned_at}
                           </td>
-                          <td>
+                          <td id="tableColor">
                             {
                               move.version_group_details[0].move_learn_method
                                 .name
